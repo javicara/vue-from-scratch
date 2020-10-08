32b30fd1997e80aece7d394bd2a74ad5998a407b
javicara <javiercaraballo97@gmail.com>
2020-10-08 12:05:46 -0300

    data binding con V model

--- components/vmodel-checkboxes.js
+++ components/vmodel-checkboxes.js
@@ -6,13 +6,14 @@ Vue.component('vmodel-checkboxes', {
     },
     template: `
       <div>
-        <h2>vmodel con arrays</h2>
+        <h2>Data binding vmodel con arrays</h2>
         <input type="checkbox" id="vuejs2" value="Vuejs 2" v-model="frameworks" />
         <label for="vuejs2">Vuejs 2</label>
         <input type="checkbox" id="reactjs" value="Reactjs" v-model="frameworks" />
         <label for="reactjs">Reactjs</label>
         <input type="checkbox" id="angular" value="Angular" v-model="frameworks" />
         <label for="angular">Angular</label>
+      >
         <p>Frameworks seleccionados: {{ frameworks }}</p>
       </div>
     `
